55dd9b2fd8e75a3b29ec8271b011bece2f02530d
pinkugoldutibu <sharolarkansas@gmail.com>
2025-11-30 09:22:00 -0600

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,17 @@
+name = "sharol"
+print (type(name))
+age = 9
+print (type(age))
+weight = 34.1
+print (type(weight))
+is_student = True
+print (type(is_student))
+print ("n\ After type casting......")
+age = str(age)
+print (type(age))
+name = str(name)
+print (type(name))
+weight = int(weight)
+print (type(weight))
+is_student = str(is_student)
+print (type(is_student))
